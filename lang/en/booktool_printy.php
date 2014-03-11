49f127384d4ee7f59ac8a313e163884de834c6a5
hrvojegolcic <hrvojegolcic@gmail.com>
2014-03-11 14:23:56 +0100

    initial commit

---
+++ lang/en/booktool_printy.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Book module language strings
+ *
+ * @package    bookhrvoje_print
+ * @copyright  2011 Petr Skoda {@link http://skodak.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$string['event_book_printed'] = 'HRBook printed';
+$string['event_chapter_printed'] = 'HRChapter printed';
+$string['pluginname'] = 'HRBook printing';
+$string['printbook'] = 'HRPrint book';
+$string['printchapter'] = 'HRPrint this chapter';
+$string['printdate'] = 'HRDate';
+$string['printedby'] = 'HRPrinted by';
+$string['print:print'] = 'HRPrint book';
